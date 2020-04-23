9ac087982734cf317c0554d95213f3743ec71c11
gvald23 <giancarlo.g.valdivia@gmail.com>
2020-04-23 15:38:40 -0400

    Finished even-disk loop algorithm

--- Algorithm.java
+++ Algorithm.java
@@ -3,6 +3,7 @@ public class Algorithm{
     private static ArrayList<Integer> col1 = new ArrayList<Integer>();
     private static ArrayList<Integer> col2 = new ArrayList<Integer>();
     private static ArrayList<Integer> col3 = new ArrayList<Integer>();
+    private static int j = 1;
     
     public static void initCol1(int numDisks){
         for (int i = numDisks; i > 0; i--){ //initializing the first "column"
@@ -10,7 +11,7 @@ public class Algorithm{
         }
     }
 
-    //always move in this order(either way): col1-col2, col1-col3, col2-col3
+    //always move in this order(either way): col1-col2, col1-col3, col2-col3 FOR EVEN DISKS
     public static void loopAlgorithm(int numDisks){
         if (numDisks%2 == 0){ //even number of disks
             while (!col1.isEmpty() || !col2.isEmpty()){
@@ -23,30 +24,63 @@ public class Algorithm{
                     col1.add(col2.get(col2.size()-1));
                     col2.remove(col2.size()-1);
                 }
-
+                else{
+                    if (col1.get(col1.size()-1) < col2.get(col2.size()-1)){
+                        col2.add(col1.get(col1.size()-1));
+                        col1.remove(col1.size()-1);
+                    }
+                    else if (col1.get(col1.size()-1) > col2.get(col2.size()-1)){
+                        col1.add(col2.get(col2.size()-1));
+                        col2.remove(col2.size()-1);
+                    }
+                }
+                
+                
                 //col1-col3
-                if (col3.isEmpty() || col1.get(col1.size()-1) < col3.get(col3.size()-1)){ //col1 to col3
+                if (col3.isEmpty()){ //col1 to col3
                     col3.add(col1.get(col1.size()-1));
                     col1.remove(col1.size()-1);
                 }
-                else if (col1.isEmpty() || col1.get(col1.size()-1) > col3.get(col3.size()-1)){ //col3 to col1
+                else if (col1.isEmpty()){ //col3 to col1
                     col1.add(col3.get(col3.size()-1));
                     col3.remove(col3.size()-1);
                 }
+                else{
+                    if (col1.get(col1.size()-1) < col3.get(col3.size()-1)){
+                        col3.add(col1.get(col1.size()-1));
+                        col1.remove(col1.size()-1); 
+                    }
+                    else if (col1.get(col1.size()-1) > col3.get(col3.size()-1)){
+                        col1.add(col3.get(col3.size()-1));
+                        col3.remove(col3.size()-1);
+                    }
+                }
 
+               
                 //col2-col3
-                if (col3.isEmpty() || col2.get(col2.size()-1) < col3.get(col3.size()-1)){ //col2 to col3
+                if (col3.isEmpty()){ //col2 to col3
                     col3.add(col2.get(col2.size()-1));
                     col2.remove(col2.size()-1);
                 }
-                else if (col2.isEmpty() || col2.get(col2.size()-1) > col3.get(col3.size()-1)){ //col3 to col2
+                else if (col2.isEmpty()){ //col3 to col2
                     col2.add(col3.get(col3.size()-1));
                     col3.remove(col3.size()-1);
+                }
+                else{
+                    if (col2.get(col2.size()-1) < col3.get(col3.size()-1)){
+                        col3.add(col2.get(col2.size()-1));
+                        col2.remove(col2.size()-1);
+                    }
+                    else if (col2.get(col2.size()-1) > col3.get(col3.size()-1)){
+                        col2.add(col3.get(col3.size()-1));
+                        col3.remove(col3.size()-1);
+                    }
                 }
             }
         }
 
-        //printing columns
+
+        //printing final columns
         for (int i = col3.size()-1; i >= 0; i--){
             System.out.println("\t\t" + " " + col3.get(i));
         }
